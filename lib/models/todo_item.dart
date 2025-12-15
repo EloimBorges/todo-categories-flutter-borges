c775ea5ec5dad41ec46825824d993927eae6b67d
EloimBorges <Borges.eloim@gmail.com>
2025-12-15 12:19:45 -0400

    Initial commit - Project setup with Provider and main.dart

---
+++ lib/models/todo_item.dart
@@ -0,0 +1,18 @@
+
+
+
+
+
+class TodoItem {
+  final String id;
+  String title;
+  bool isDone;
+  DateTime? dueDate;
+
+  TodoItem({
+    required this.id,
+    required this.title,
+    this.isDone = false,
+    this.dueDate,
+  });
+}
